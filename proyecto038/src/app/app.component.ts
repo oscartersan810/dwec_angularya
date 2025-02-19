a5576c807fae6902dac890fd758c522044e3c7b3
oscartersan810 <oscartersan810@gmail.com>
2025-02-19 10:01:43 +0100

    Instalación y Aplicar Boostrap

--- proyecto038/src/app/app.component.ts
+++ proyecto038/src/app/app.component.ts
@@ -1,11 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
+declare var bootstrap: any;
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   standalone: false,
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'proyecto038';
+
+  ngOnInit(): void {
+    bootstrap('[data-bs-toggle="popover"]').popover();
+ }
+
+
+ aceptar(){
+  alert("Ha pulsado Aceptar!");
+}
+
+cancelar(){
+  alert("Ha pulsado Cancelar!");
+}
 }
